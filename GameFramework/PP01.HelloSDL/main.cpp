70ba53b447105c27ba869f62322e551ccc4bc580
선아 신 <1706abc@naver.com>
2018-11-13 15:33:43 +0900

    PP04.BasicDrawing

--- GameFramework/PP01.HelloSDL/main.cpp
+++ GameFramework/PP01.HelloSDL/main.cpp
@@ -7,7 +7,7 @@ Game*g_game = 0;
 int main(int argc, char* argv[])
 {
 	g_game = new Game();
-	g_game->init("Chapter 3", 100, 100, 640, 480, false);
+	g_game->init("Chapter 4", 100, 100, 640, 480, false);
 
 	while (g_game->running())
 	{
@@ -15,7 +15,7 @@ int main(int argc, char* argv[])
 		g_game->update();
 		g_game->render();
 	}
-
+	SDL_Delay(5000);
 	g_game->clean();
 	return 0;
 }
