ef04177dcc9bf80fc8c4773be9d99facad2f41d5
shinseona <20171189@game.hoseo.edu>
2018-11-15 17:13:09 +0900

    push test

--- GameFramework/PP01.HelloSDL/main.cpp
+++ GameFramework/PP01.HelloSDL/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char* argv[])
 		g_game->handleEvents();
 		g_game->update();
 		g_game->render();
-		SDL_Delay(10); // add the delayy
+		SDL_Delay(9); // add the delayy
 	}
 
 	g_game->clean();
